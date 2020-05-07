c0cbe0f94e5d627516953b9474e718c0272889f8
stevenwilliamolthoff <stevenolthoff@gmail.com>
2020-05-07 14:32:45 -0400

    make button open info

--- src/operations/index.tsx
+++ src/operations/index.tsx
@@ -2,7 +2,18 @@ import React from "react"
 import Map from "../map"
 import "./operations.css"
 
-export default class Operations extends React.Component<any> {
+export default class Operations extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      showOperationInfo: false,
+    }
+  }
+
+  onNewOperationButtonClick() {
+    this.setState({ showOperationInfo: true })
+  }
+
   render() {
     return (
       <div className='operations'>
@@ -10,13 +21,18 @@ export default class Operations extends React.Component<any> {
           <div className='operations__toolbar__search'>
             Search Location by name or coordinates
           </div>
-          <div className='operations__toolbar__new-operation-button'>
+          <div
+            className='operations__toolbar__new-operation-button'
+            onClick={() => this.onNewOperationButtonClick()}
+          >
             + New Operation
           </div>
         </div>
         <div className='operations__map'>
           <Map></Map>
-          <div className='operations__map__info'></div>
+          {this.state.showOperationInfo ? (
+            <div className='operations__map__info'></div>
+          ) : null}
         </div>
       </div>
     )
